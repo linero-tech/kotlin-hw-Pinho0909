32bebb16eeda7f11ad800020d2431c6193250cae
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-01-24 16:52:09 +0100

    Solution Task 4

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task4(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT X
+        INPUT Y
+            IF X > Y THEN
+        OUTPUT "X"
+            ELSE
+            IF Y > X THEN
+        OUTPUT "Y"
+            ElSE
+        OUTPUT "X" "=" "Y"
+    """.trimIndent()
 }
