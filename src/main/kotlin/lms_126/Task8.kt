b326d91c1614cdeb4bf134f73fe554c841675869
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-01-25 23:53:15 +0100

    Solution Task 8

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -1,6 +1,19 @@
+
+
 package lms_126
 
 fun task8(): String {
-
-    return TODO("Replace this 'TODO' with your solution")
+    return"""
+        INPUT X
+            If X is multiple of 3 THEN 
+                If X is multiple of 5 THEN 
+                    OUTPUT "FooBar"
+                ELSE         
+                    OUTPUT "Foo"
+            ELSE
+                If X is not multiple of 5 THEN
+                    OUTPUT "Qix"
+                ELSE
+                    OUTPUT "Bar"
+    """.trimIndent()
 }
