dce23c32640ed01be54d7aa9f9b03eff48b5dd7c
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-01-25 23:52:25 +0100

    Solution Task 5

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,16 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT X = 1 to 8
+            WHEN X =
+                    1-> OUTPUT "Monday"       
+                    2-> OUTPUT "Tuesday"
+                    3-> OUTPUT "Wednesday"
+                    4-> OUTPUT "Thursday"       
+                    5-> OUTPUT "Friday"
+                    6-> OUTPUT "Saturday"
+                    7-> OUTPUT "Sunday"
+                    8-> OUTPUT "Next Week"
+    """.trimIndent()
 }
