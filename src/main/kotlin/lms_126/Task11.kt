32e7bad500a21c3ea5e157fef82132c68078fecc
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-01-27 12:28:42 +0100

    Solution Task 11

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,22 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+         START
+         INPUT temperature (limit with 3 characters)
+         READ temperature characters
+            IF third characters = C THEN
+            READ temperature characters
+            First and Second characters convert to integral number = c
+                f = 1.8 * c + 32
+                OUTPUT f
+                END
+            ELSE
+            READ temperature characters
+            First and Second characters convert to integral number = f
+            c = ( f - 32 ) * 0.5556
+            OUTPUT c
+            END
+    """.trimIndent()
+
 }
