eeb67319c762b577325334c4b90c390c8a021a17
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-01-27 12:28:36 +0100

    Solution Task 10

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,21 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+         START
+         OUTPUT "Write your new account with your email address"
+         INPUT email
+         OUTPUT "Write your Passwords with 6 characters "
+         INPUT pass (limit with 6 characters)
+         OUTPUT "Write your Passwords again "
+         INPUT pass2 (limit with 6 characters)
+            IF pass different pass 2 THEN
+                OUTPUT "The Passwords are not the same.Try Create you account again, Thanks "
+                END    
+            ELSE  
+            INPUT email
+            INPUT pass
+            OUTPUT "You created your account with success"
+            END
+    """.trimIndent()
 }
