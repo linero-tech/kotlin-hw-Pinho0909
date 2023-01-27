0953a41f888236732bdda3f423bf3b0807aad1e6
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-01-27 01:06:27 +0100

    Solution Task 9

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,35 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        START 
+        OUTPUT "Hello Insert your Card"
+        IF acceptable card THEN
+            OUTPUT "Write your Password"
+            INPUT password   
+            IF password is correct THEN
+                OUTPUT "Write the Amount to Withdraw"
+                INPUT amount 
+                    IF amount < Account Balance THEN
+                        OUTPUT "Operation is Canceled and Take your Card"   
+                        END
+                    ELSE IF amount > limit THEN         
+                        OUTPUT "Maximum Amount is limit and Take your Card"   
+                        END
+                    ELSE
+                    OUTPUT "Do you want Receipt 
+                    INPUT x
+                        IF x = yes
+                            OUTPUT "Take your Amount, Card and Receipt"    
+                            END
+                        ELSE
+                            OUTPUT "Take your Amount and Card"
+                            END
+            ELSE   
+            OUTPUT "Password Incorrect and Take your Card"   
+            END        
+        ELSE
+        OUTPUT "It is not Acceptable Card and Take your Card"
+        END
+    """.trimIndent()
 }
