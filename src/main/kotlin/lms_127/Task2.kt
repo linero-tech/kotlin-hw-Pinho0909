e2cf60de1bf208d7de66a255bd93465a64f07e26
Pinho0909 <122530757+Pinho0909@users.noreply.github.com>
2023-02-14 18:57:13 +0100

    Update Task 2

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -1,9 +1,11 @@
 package lms_127
 
-
-fun task2 (name:String,country:String,month:String) {
-    println("Hello $name, you are from $country and you were born in $month")
+fun task2 () {
+    val name="Jane"
+    val country ="Sweden"
+    val month="June"
+    print("Hello $name, you are from $country and you were born in $month")
 }
 fun main(){
-    task2("Jane","Sweden","June" )
+    task2()
 }
