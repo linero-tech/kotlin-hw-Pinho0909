e7695a83213a9cda059ea73971419853adb4b376
Pinho0909 <122530757+Pinhodev@users.noreply.github.com>
2023-02-19 15:23:11 +0100

    Solution Task 5

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -1,6 +1,18 @@
 package lms_129
 
 fun task5(sentence: String): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    //for (i  in 0 until sentence.length)
+    for (i  in sentence.indices)
+    {
+        result += 1
+    }
+    return result
+}
+fun main (){
+    val sentence = "I love GBG"
+    println("Assume that")
+    println("¤ sentence is $sentence")
+    println("Then")
+    println("¤ result is " + task5(sentence))
 }
