33a09e62dc440690a75345848ed7dd2a04c68704
Pinho0909 <122530757+Pinhodev@users.noreply.github.com>
2023-02-21 22:09:30 +0100

    Solution Task 9

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,20 @@
 package lms_129
-
+import kotlin.math.roundToInt
 fun task9(temperature: String): String {
+    val num = temperature.dropLast(1).toInt()
+    val temp = temperature.last().uppercase()
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = if (temp == "C") {
+        (((1.8 * num + 32) * 100.0).roundToInt() /100).toString()+"F"
+    }
+    else ((((num - 32) * 0.5556) * 100.0).roundToInt() /100).toString()+"C"
+    return (result)
 }
+fun main (){
+    val temperature= "15c"
+    println("Assume that")
+    println("¤ temperature is $temperature")
+    println("Then")
+    println("¤ result is " + task9(temperature))
+}
+
