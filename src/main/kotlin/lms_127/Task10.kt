b2bb89f1d84949b79ccaee44d5a44c5cd2502784
Pinho0909 <122530757+Pinhodev@users.noreply.github.com>
2023-02-23 18:25:52 +0100

    Update Modulo 102

--- src/main/kotlin/lms_127/Task10.kt
+++ src/main/kotlin/lms_127/Task10.kt
@@ -1,20 +1,15 @@
 package lms_127
 
 fun task10_1(assessments: String): Int {
-
     return assessments.length
 }
 fun task10_2(assessments: String): Char {
-
     return assessments[3]
 }
 fun task10_3(assessments: String): Char {
-    val result = assessments.length/2
-    return assessments[result]
+    return assessments[assessments.length/2]
 }
-
 fun task10_4(assessments: String): String {
-
     return assessments.substring(0,3)
 }
 fun main(){
