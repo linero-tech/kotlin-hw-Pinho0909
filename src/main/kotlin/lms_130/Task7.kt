2071b3426da91f502be70230175346c5f2c827ba
Pinho0909 <122530757+Pinhodev@users.noreply.github.com>
2023-03-14 00:08:45 +0100

    Update Task 7 and 8

--- src/main/kotlin/lms_130/Task7.kt
+++ src/main/kotlin/lms_130/Task7.kt
@@ -4,22 +4,19 @@ import kotlin.math.min
 
 fun task7(items: List<Int>): Int {
     
-    var minValue = 0
-    var count = 0
+    var minValue = items.first()
     
-    while (count < items.size - 1) {
-        minValue = if (items[count] < items[count + 1]) {
-            items[count]
-        } else {
-            items[count + 1]
+    items.forEach { number ->
+        if (number < minValue) {
+            minValue = number
         }
-        count += 1
     }
     return minValue
 }
+    ///Option 2 = return items.sorted()[0]
 
 fun main() {
-    val myList = listOf(10, 5, 11)
+    val myList = listOf(-1, 0, 1, 2, 3, 4, 5, -9, -10, 0, 100)
     println("¤ items is $myList")
     println("Then")
     println("¤ result is " + task7(myList))
--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -2,11 +2,12 @@ package lms_130
 
 fun task8(items: List<Int>): List<Int> {
     
-    return items.sortedDescending().subList(0, 3).reversed()
+    val  highestValue = items.sortedDescending().subList(0, 3).reversed()
+    return highestValue.toSet().toList()
 }
 
 fun main() {
-    val myList = listOf(60, 9, 7, 10)
+    val myList = listOf(1, 1, 1, 1)
     println("¤ items is $myList")
     println("Then")
     println("¤ result is " + task8(myList))
