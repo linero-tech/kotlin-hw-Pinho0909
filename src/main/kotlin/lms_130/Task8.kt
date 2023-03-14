23fba0e049f3290a47d1e282a46fd18bfe149171
Pinho0909 <122530757+Pinhodev@users.noreply.github.com>
2023-03-14 01:34:17 +0100

    Update Task 8

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -2,11 +2,11 @@ package lms_130
 
 fun task8(items: List<Int>): List<Int> {
     
-    return items.sortedDescending().toSet().toList().subList(0,3).reversed()
+    return items.toSet().sortedDescending().subList(0,3).reversed()
 }
 
 fun main() {
-    val myList = listOf(60, 9, 7, 10)
+    val myList = listOf(60, 9, 7, 10, 9)
     println("¤ items is $myList")
     println("Then")
     println("¤ result is " + task8(myList))
