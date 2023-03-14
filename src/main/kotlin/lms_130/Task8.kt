44f9b386b86f61576470c7dae48b5bfd834df29d
Pinho0909 <122530757+Pinhodev@users.noreply.github.com>
2023-03-14 10:32:43 +0100

    Update Task 8

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -2,11 +2,22 @@ package lms_130
 
 fun task8(items: List<Int>): List<Int> {
     
-    return items.toSet().sortedDescending().subList(0,3).reversed()
+    //Option 2
+    val highestList = items.sortedDescending()
+    val highestValues = mutableSetOf<Int>()
+    
+    for (index in 0..2) {
+        highestValues.add(highestList[index])
+    }
+    println("Option 2 is " + highestValues.reversed())
+    
+    //My Solution
+    val result = items.sortedDescending().subList(0, 3).reversed()
+    return (result.toSet().toList())
 }
 
 fun main() {
-    val myList = listOf(60, 9, 7, 10, 9)
+    val myList = listOf(60, 9, 7, 10)
     println("¤ items is $myList")
     println("Then")
     println("¤ result is " + task8(myList))
